8d80554888303327e9dc7d76024f037ab6ee1d71
KannaChan <adri.94b@gmail.com>
2019-06-03 16:08:19 +0200

    comLed

---
+++ 04_morse/morse_local/ledArduino.py
@@ -0,0 +1,24 @@
+import i2cComm
+import time
+dico = { "a" : [[1,1,1,1,1],
+                [1,0,0,0,1],
+                [1,1,1,1,1],
+                [1,0,0,0,1],
+                [1,0,0,0,1]],
+
+         "b" : [[1,1,1,1,0],
+                [1,0,0,0,1],
+                [1,1,1,1,0],
+                [1,0,0,0,1],
+                [1,1,1,1,0]],
+}
+
+def envoiCaractere():
+
+    matrice = dico["a"]
+    for ligne in matrice:
+        for led in ligne :
+            i2cComm.writeNumber(led)
+
+print("on va afficher la lettre a")
+envoiCaractere()
